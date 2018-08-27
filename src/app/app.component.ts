1cadcb1c7e4e34f18cf3a24bfcf436abeab3e650
Alejandro Juarez <alejandro.juarez8612@gmail.com>
2018-08-26 17:03:33 -0700

    Integrated random words library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as randomWords from 'random-words';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tpg-fe';
+  title = randomWords(1);
 }
