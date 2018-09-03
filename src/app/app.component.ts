d0d1952e0de4dc4aa430616f26b2458d6c4a3750
Alejandro Juarez <alejandro.juarez8612@gmail.com>
2018-09-02 23:20:38 -0700

    Upgraded to Angular 6 and did some code refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,15 @@
-import { Component, ViewChild, ElementRef, OnInit, AfterViewInit} from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit, ViewEncapsulation} from '@angular/core';
 import * as randomWords from 'random-words';
-import { Score } from './models/score.model';
-import { MistypedChar } from './models/mistyped.model';
+import { Score } from './shared/score.model';
+import { MistypedChar } from './shared/mistyped.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  timer;
-  timeLeft;
-  pressedKeyCode;
-  globalAccuracy;
-  golablWordsTyped;
-
   scoring = {
     gameScore: [],
     mistypes: []
@@ -22,11 +17,17 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   stats = [];
 
-  typedWord : string;
+  typedWord: string;
+  displayedWord: string;
   typingButtonLabel = "Start typing!"
   randomWord = randomWords(1)[0];
 
+  timer;
+  timeLeft: number;
+  globalAccuracy: number;
+  golablWordsTyped: number;
   currWordLength: number;
+  correctChars = 0;
   correctWords = 0;
   accuracy = 100;
   baseTime = 10;
@@ -41,6 +42,7 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.timeLeft = this.baseTime;
+    this.displayedWord = this.randomWord;
     this.currWordLength = this.randomWord.length;
   }
 
@@ -86,8 +88,9 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   checkWord(event) {
-    debugger;
-    if(event.inputType === "deleteContentBackward") return;
+    if(event.inputType === "deleteContentBackward") { 
+      return;
+    };
 
     if (!this.gameStarted) {
       this.gameStarted = true;
@@ -105,8 +108,10 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   updateWord() {
     this.randomWord = randomWords(1)[0];
+    this.displayedWord = this.randomWord;
     this.currWordLength = this.randomWord.length;
     this.typedWord = "";
+    this.correctChars = 0;
     this.failures = 0;
     this.accuracy = 100;
   }
@@ -124,9 +129,12 @@ export class AppComponent implements OnInit, AfterViewInit {
     let typed = this.typedWord.substring(this.typedWord.length - 1)
 
     if (this.randomWord.substring(0, currTypedIndex) == this.typedWord) {
+      this.correctChars = this.correctChars + 1;
+      this.highlightCorrect();
       return true;
     }
     
+    this.highlightMistake();
     this.scoring.mistypes.push(new MistypedChar(0, goal, typed));
     return false;
   }
@@ -146,7 +154,24 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.globalAccuracy = Math.round(acc / this.golablWordsTyped);
   }
 
-  keyPressed(event) {
-    this.pressedKeyCode = event.keyCode;
+  highlightCorrect() {
+    let firstPart = this.randomWord.substring(0, this.typedWord.length);
+    let lastPart = this.randomWord.substring(this.typedWord.length);
+
+    this.displayedWord = '<span class=correctHighlight>' + firstPart + '</span>' + lastPart;     
+  }
+
+  highlightMistake() {
+    debugger;
+    let mistakes = this.typedWord.length - this.correctChars;
+
+    let firstPart = this.randomWord.substring(0, this.correctChars);
+    let error = this.randomWord.substring(firstPart.length, firstPart.length + mistakes);
+    let lastPart = this.randomWord.substring(firstPart.length + error.length);
+
+    let correctSpan = '<span class=correctHighlight>' + firstPart + '</span>';
+    let errorSpan = '<span class=mistakeHighlight>' + error + '</span>';
+
+    this.displayedWord = correctSpan + errorSpan + lastPart;     
   }
 }
